9c3014d4831c40eb85fed72a57c1df49f65a2dc4
wangyj <wangyj@niaodaifu.cn>
2017-08-24 16:59:29 +0800

    add comment

--- lib/db.php
+++ lib/db.php
@@ -1,4 +1,5 @@
 <?php
 
+//访问数据库
 $pdo = new PDO('mysql:host=localhost;dbname=restful','root','');
 return $pdo;
--- models/Status.php
+++ models/Status.php
@@ -6,17 +6,17 @@
  */
 class Status
 {
-    public static $USERNAME_EMPTY = 1;
+    public static $USERNAME_EMPTY = 1;  //用户名为空
 
-    public static $PASSWORD_EMPTY = 2;
+    public static $PASSWORD_EMPTY = 2;  //密码为空
 
-    public static $USER_NOT_EXISTS = 3;
+    public static $USER_NOT_EXISTS = 3; //用户不存在
 
-    public static $USER_REGISTER_FAILD = 4;
+    public static $USER_REGISTER_FAILD = 4; //注册失败
 
-    public static $NOT_ALLOWED_METHOD = 403;
+    public static $NOT_ALLOWED_METHOD = 403; //请求方法不允许
 
-    public static $NOT_FOUND = 404;
+    public static $NOT_FOUND = 404;  //请求资源不存在
 
 
     public static $DESC = [
@@ -24,7 +24,7 @@ class Status
         2 => 'password empty',
         3 => 'user not exists',
         4 => 'user register failed',
-        
+
         403 => 'forbbiden',
         404 => 'not found',
         401 => 'not auth',
